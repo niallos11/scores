feea67bcb49d06f213ac14d1c3ed5f07dc5ba21c
Niall <niallos11@gmail.com>
2022-06-06 21:52:11 +0000

    Add error handings for score integer imput,add termcolor to colour terminal txt,updated requirments.txt

--- run.py
+++ run.py
@@ -1,35 +1,40 @@
 # import pyfiglet module
-import pyfiglet
-  
-result = pyfiglet.figlet_format("SCORES")
-print(result)
-
-import gspread
 from google.oauth2.service_account import Credentials
+from termcolor import colored
+import gspread
+import pyfiglet
+
+result = pyfiglet.figlet_format("SCORES") 
+print(result)
+print(colored("Check your score", 'green'))
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('scores')
 
+
 def get_scores_data():
     """
     Get name and team input from the user.
     """
-    name_str = input("What is  your name:\n")
+    name_str = input("What is your name:\n")
     print(f"Your name is {name_str}")
 
-    team_str = input("What is  your team you support:\n")
+    team_str = input("What team are you on:\n")
     print(f"Your team is {team_str}")
-
-    score_str = input("What score did you achieve:\n")
-    print(f"Your score was {team_str}")
 
 
 get_scores_data()
+
+try:
+    score_str = int(input("Enter a score:\n"))
+    print(f"Your score is {score_str}")
+except (SyntaxError, ValueError):
+    print("You did not enter a score")
