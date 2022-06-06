e1f7cc3067012a3641b57bdd517274c3fdae503b
Niall <niallos11@gmail.com>
2022-06-06 00:05:26 +0000

    add figlet ASCII font for terminal

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+# import pyfiglet module
+import pyfiglet
+  
+result = pyfiglet.figlet_format("SCORES")
+print(result)
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -16,11 +22,14 @@ def get_scores_data():
     """
     Get name and team input from the user.
     """
-    name_str = input("What is  your name::\n")
+    name_str = input("What is  your name:\n")
     print(f"Your name is {name_str}")
 
     team_str = input("What is  your team you support:\n")
     print(f"Your team is {team_str}")
+
+    score_str = input("What score did you achieve:\n")
+    print(f"Your score was {team_str}")
 
 
 get_scores_data()
