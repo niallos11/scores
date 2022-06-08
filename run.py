0579eeb3c94d8b1836f0a03685ef1d728baa4f63
Niall <niallos11@gmail.com>
2022-06-08 23:10:04 +0000

    add validation check for values and error handling if number is not provided

--- run.py
+++ run.py
@@ -34,8 +34,39 @@ def main_menu():
 
 def get_scores_data():
     """
-    Get name and team input from the user.
+    Get scores figures input from the user.
+    Run a while loop to collect a valid string of data
     """
+    while True:
+
+        data_str = input("Enter your data here:\n")
+
+        scores_data = data_str.split(",")
+
+        if validate_data(scores_data):
+            print("Data is valid! Enter again to proceed!")
+            break
+
+    return scores_data
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 1:
+            raise ValueError(
+                f"Exactly 1 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
     name_str = input("What is your name:\n")
     print(f"Your name is {name_str}")
 
@@ -51,6 +82,9 @@ try:
 except (SyntaxError, ValueError):
     print("You did not enter a score")
 
+
 def view_scores_data():
-    print("this shows the score data")
+    print("this will show the score data")
+
+
 main_menu()
