4e12a4858df31fc1570492cbd303cc3d7ee1decb
Niall <niallos11@gmail.com>
2022-06-10 20:14:11 +0000

    cleanup code and fixed error

--- run.py
+++ run.py
@@ -65,11 +65,11 @@ def get_scores_data():
 
 def validate_data(values):
     """
-    Inside the try, converts all string values into integers.
+    Inside the try statement this converts all string values into integers.
     Raises ValueError if strings cannot be converted into int.
     """
     try:
-        [int(value) for value in values]
+        values = [int(value) for value in values]
         if len(values) != 1:
             raise ValueError(
                 f"Exactly 1 values required, you provided {len(values)}"
